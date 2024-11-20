61b53836bf23cc4951260a0b6a45f325fb936d8e
Shion <smatsumoto97@gmail.com>
2024-11-19 21:18:50 -0500

    clean up

--- unlearn_mnist.py
+++ unlearn_mnist.py
@@ -10,7 +10,11 @@ from pathlib import Path
 from copy import deepcopy
 
 
+DATA_DIR = "./data"
 MODEL_DIR = "./models"
+BATCH_SIZE = 64
+EPOCHS = 5
+LEARNING_RATE = 1e-3
 
 
 class SimpleNet(nn.Module):
@@ -26,44 +30,6 @@ class SimpleNet(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-
-
-BATCH_SIZE = 64
-EPOCHS = 5
-LEARNING_RATE = 1e-3
-
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-)
-
-train_dataset = datasets.MNIST(
-    root="./data", train=True, download=True, transform=transform
-)
-
-test_dataset = datasets.MNIST(
-    root="./data", train=False, download=True, transform=transform
-)
-
-train_dataset, val_dataset = random_split(train_dataset, [0.9, 0.1])
-
-# Split training dataset into retain and forget subsets
-forget_class = 5
-forget_idx = np.where(
-    train_dataset.dataset.targets[train_dataset.indices] == forget_class
-)[0]
-retain_idx = np.where(
-    train_dataset.dataset.targets[train_dataset.indices] != forget_class
-)[0]
-
-forget_dataset = Subset(train_dataset, forget_idx)
-retain_dataset = Subset(train_dataset, retain_idx)
-
-# Create dataloaders for each dataset split
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-forget_loader = DataLoader(forget_dataset, batch_size=BATCH_SIZE, shuffle=True)
-retain_loader = DataLoader(retain_dataset, batch_size=BATCH_SIZE, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 
 def train(model, dataloader, criterion, optimizer, epochs):
@@ -167,66 +133,103 @@ def unlearn(
         )
 
 
-# Set model paths
-trained_model_path = Path(f"{MODEL_DIR}/trained_mnist.pt")
-retrained_model_path = Path(f"{MODEL_DIR}/retrained_mnist.pt")
-criterion = nn.CrossEntropyLoss()
+def main():
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+
+    train_dataset = datasets.MNIST(
+        root=DATA_DIR, train=True, download=True, transform=transform
+    )
+
+    test_dataset = datasets.MNIST(
+        root=DATA_DIR, train=False, download=True, transform=transform
+    )
+
+    train_dataset, val_dataset = random_split(train_dataset, [0.9, 0.1])
+
+    # Split training dataset into retain and forget subsets
+    forget_class = 5
+    forget_idx = np.where(
+        train_dataset.dataset.targets[train_dataset.indices] == forget_class
+    )[0]
+    retain_idx = np.where(
+        train_dataset.dataset.targets[train_dataset.indices] != forget_class
+    )[0]
+
+    forget_dataset = Subset(train_dataset, forget_idx)
+    retain_dataset = Subset(train_dataset, retain_idx)
+
+    # Create dataloaders for each dataset split
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    forget_loader = DataLoader(forget_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    retain_loader = DataLoader(retain_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+    # Set model paths
+    trained_model_path = Path(f"{MODEL_DIR}/trained_mnist.pt")
+    retrained_model_path = Path(f"{MODEL_DIR}/retrained_mnist.pt")
+    criterion = nn.CrossEntropyLoss()
+
+    # Train with full dataset
+    print("=== Standard training ===")
+    trained_model = SimpleNet()
+    if trained_model_path.exists():
+        print(f"Loading pretrained model from {trained_model_path}.")
+        trained_model.load_state_dict(torch.load(trained_model_path, weights_only=True))
+    else:
+        optimizer = optim.Adam(trained_model.parameters(), lr=LEARNING_RATE)
+        train(trained_model, train_loader, criterion, optimizer, EPOCHS)
+
+        print(f"Saving trained model to {trained_model_path}.")
+        torch.save(trained_model.state_dict(), trained_model_path)
+
+    trained_acc = evaluate(trained_model, test_loader)
+    print(f"Trained accuracy: {trained_acc}")
+
+    # Retrain with retain dataset (gold standard)
+    print("\n=== Retrain on retain dataset (gold standard) ===")
+    retrained_model = SimpleNet()
+    if retrained_model_path.exists():
+        print(f"Loading retrained model from {retrained_model_path}.")
+        retrained_model.load_state_dict(
+            torch.load(retrained_model_path, weights_only=True)
+        )
+    else:
+        optimizer = optim.Adam(retrained_model.parameters(), lr=LEARNING_RATE)
+        train(retrained_model, retain_loader, criterion, optimizer, EPOCHS)
+
+        print(f"Saving retrained model to {retrained_model_path}.")
+        torch.save(retrained_model.state_dict(), retrained_model_path)
+
+    retrained_acc = evaluate(retrained_model, test_loader)
+    print(f"Retrained accuracy: {retrained_acc}")
+
+    # Unlearning with KL divergence loss (with retain step)
+    print("\n=== Finetune with KLDiv loss (with retain step) ===")
+    unlearned_model = deepcopy(trained_model)
+
+    unlearned_initial_acc = evaluate(unlearned_model, test_loader)
+    print(f"Trained accuracy: {unlearned_initial_acc}")
+
+    forget_optimizer = optim.Adam(unlearned_model.parameters(), lr=LEARNING_RATE)
+    retain_optimizer = optim.Adam(unlearned_model.parameters(), lr=LEARNING_RATE)
+
+    unlearn(
+        unlearned_model,
+        retain_loader,
+        forget_loader,
+        val_loader,
+        retain_optimizer,
+        forget_optimizer,
+        EPOCHS,
+        retain_loop=True,
+    )
+
+    unlearned_acc = evaluate(unlearned_model, test_loader)
+    print(f"Unlearned accuracy (with retain step): {unlearned_acc}")
 
 
-# Train with full dataset
-print("=== Standard training ===")
-trained_model = SimpleNet()
-if trained_model_path.exists():
-    print(f"Loading pretrained model from {trained_model_path}.")
-    trained_model.load_state_dict(torch.load(trained_model_path, weights_only=True))
-else:
-    optimizer = optim.Adam(trained_model.parameters(), lr=LEARNING_RATE)
-    train(trained_model, train_loader, criterion, optimizer, EPOCHS)
-
-    print(f"Saving trained model to {trained_model_path}.")
-    torch.save(trained_model.state_dict(), trained_model_path)
-
-trained_acc = evaluate(trained_model, test_loader)
-print(f"Trained accuracy: {trained_acc}")
-
-
-# Retrain with retain dataset (gold standard)
-print("\n=== Retrain on retain dataset (gold standard) ===")
-retrained_model = SimpleNet()
-if retrained_model_path.exists():
-    print(f"Loading retrained model from {retrained_model_path}.")
-    retrained_model.load_state_dict(torch.load(retrained_model_path, weights_only=True))
-else:
-    optimizer = optim.Adam(retrained_model.parameters(), lr=LEARNING_RATE)
-    train(retrained_model, retain_loader, criterion, optimizer, EPOCHS)
-
-    print(f"Saving retrained model to {retrained_model_path}.")
-    torch.save(retrained_model.state_dict(), retrained_model_path)
-
-retrained_acc = evaluate(retrained_model, test_loader)
-print(f"Retrained accuracy: {retrained_acc}")
-
-
-# Unlearning with KL divergence loss (with retain step)
-print("\n=== Finetune with KLDiv loss (with retain step) ===")
-unlearned_model = deepcopy(trained_model)
-
-unlearned_initial_acc = evaluate(unlearned_model, test_loader)
-print(f"Trained accuracy: {unlearned_initial_acc}")
-
-forget_optimizer = optim.Adam(unlearned_model.parameters(), lr=LEARNING_RATE)
-retain_optimizer = optim.Adam(unlearned_model.parameters(), lr=LEARNING_RATE)
-
-unlearn(
-    unlearned_model,
-    retain_loader,
-    forget_loader,
-    val_loader,
-    retain_optimizer,
-    forget_optimizer,
-    EPOCHS,
-    retain_loop=True,
-)
-
-unlearned_acc = evaluate(unlearned_model, test_loader)
-print(f"Unlearned accuracy (with retain step): {unlearned_acc}")
+if __name__ == "__main__":
+    main()
