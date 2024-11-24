1740d920c2b1808c782c1d1102bcaaf9e0c16f6b
Shion <smatsumoto97@gmail.com>
2024-11-24 17:31:17 -0500

    split test into test and val. shuffle for train, forget, retain.

--- unlearning_datamodule.py
+++ unlearning_datamodule.py
@@ -40,19 +40,19 @@ class UnlearningDataModule:
         raise NotImplementedError("Must implement `setup`.")
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.ds_train, batch_size=self.batch_size)
+        return DataLoader(self.ds_train, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.ds_val, batch_size=self.batch_size)
+        return DataLoader(self.ds_val, batch_size=self.batch_size, shuffle=False)
 
     def test_dataloader(self) -> DataLoader:
-        return DataLoader(self.ds_test, batch_size=self.batch_size)
+        return DataLoader(self.ds_test, batch_size=self.batch_size, shuffle=False)
 
     def forget_dataloader(self) -> DataLoader:
-        return DataLoader(self.ds_forget, batch_size=self.batch_size)
+        return DataLoader(self.ds_forget, batch_size=self.batch_size, shuffle=True)
 
     def retain_dataloader(self) -> DataLoader:
-        return DataLoader(self.ds_retain, batch_size=self.batch_size)
+        return DataLoader(self.ds_retain, batch_size=self.batch_size, shuffle=True)
 
 
 class MNISTUnlearningDataModule(UnlearningDataModule):
@@ -127,13 +127,15 @@ class CIFAR10UnlearningDataModule(UnlearningDataModule):
             root=self.data_dir, train=False, download=True, transform=self.transform
         )
 
-        self.ds_train, self.ds_val = random_split(self.ds_train, [0.9, 0.1])
-
         # Split training dataset into retain and forget subsets
-        forget_mask = torch.isin(torch.tensor(self.ds_train.dataset.targets), torch.tensor(self.forget_class))
+        forget_mask = torch.isin(
+            torch.tensor(self.ds_train.targets), torch.tensor(self.forget_class)
+        )
 
         self.forget_idx = forget_mask.nonzero().flatten().tolist()
         self.retain_idx = (~forget_mask).nonzero().flatten().tolist()
 
         self.ds_forget = Subset(self.ds_train, self.forget_idx)
         self.ds_retain = Subset(self.ds_train, self.retain_idx)
+
+        self.ds_val, self.ds_test = random_split(self.ds_train, [0.5, 0.5])
