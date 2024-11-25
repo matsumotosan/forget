c9309c3ee2b9c0e9fc6fc4a54c1527dcf1432595
Shion <smatsumoto97@gmail.com>
2024-11-24 21:53:54 -0500

    save fig

--- plot_results.py
+++ plot_results.py
@@ -12,7 +12,7 @@ def main(args):
     data = []
     for epoch_idx, accuracies in enumerate(metrics["val_acc"]):
         for class_idx, accuracy in enumerate(accuracies):
-            data.append({'Epoch': epoch_idx, 'Class': f'Class {class_idx}', 'Accuracy': accuracy})
+            data.append({'Epoch': epoch_idx, 'Class': f'{class_idx}', 'Accuracy': accuracy})
 
     df = pd.DataFrame(data)
     if params["forget_step"] and not params["retain_step"]:
@@ -20,14 +20,15 @@ def main(args):
     if params["retain_step"] and not params["forget_step"]:
         setting = "retain"
     else:
-        setting = "forget_retain"
+        setting = "forget+retain"
 
+    fig, ax = plt.subplots(figsize=(15, 8))
     sns.lineplot(data=df, x='Epoch', y='Accuracy', hue='Class', marker='o')
 
     plt.title(f"MNIST Per-Class Accuracy over Unlearning Epochs ({setting})")
     plt.xticks(df["Epoch"])
     plt.ylabel("Accuracy")
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.grid()
     plt.show()
     # plt.savefig(f"{args.fig_dir}/unlearn_mnist_{setting}.png", dpi=300)
