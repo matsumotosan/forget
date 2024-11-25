b3ce4f1a96008d9b66094fc095e7423dc50e1b94
Shion <smatsumoto97@gmail.com>
2024-11-25 00:55:08 -0500

    plot initial trained metrics

--- plot_results.py
+++ plot_results.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 import pandas as pd
 import seaborn as sns
@@ -6,10 +7,11 @@ from utils import read_json
 
 
 def main(args):
+    os.makedirs(args.fig_dir, exist_ok=True)
     sns.set_theme()
 
-    metrics = read_json(f"{args.exp_dir}/metrics.json")
     params = read_json(f"{args.exp_dir}/params.json")
+    metrics = read_json(f"{args.exp_dir}/metrics.json")
 
     data = []
     for epoch_idx, accuracies in enumerate(metrics["val_acc"]):
@@ -27,18 +29,20 @@ def main(args):
     fig, ax = plt.subplots(figsize=(15, 8))
     sns.lineplot(data=df, x='Epoch', y='Accuracy', hue='Class', marker='o')
 
-    plt.title(f"MNIST Per-Class Accuracy over Unlearning Epochs ({setting})")
+    plt.title(f"{params['dataset'].upper()} Per-Class Accuracy Over Unlearning Epochs ({setting})")
     plt.xticks(df["Epoch"])
     plt.ylabel("Accuracy")
-    # plt.tight_layout()
+    plt.tight_layout()
     plt.grid()
     plt.show()
-    # plt.savefig(f"{args.fig_dir}/unlearn_mnist_{setting}.png", dpi=300)
+
+    filename = f"{args.fig_dir}/unlearn_{params['dataset']}_{setting}_{os.path.basename(os.path.normpath(args.exp_dir))}.png"
+    plt.savefig(filename, dpi=300)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--exp_dir", type=str, help="Experiment directory")
-    parser.add_argument("--fig_dir", type=str, help="Figure directory", default="./figures")
+    parser.add_argument("--fig_dir", type=str, help="Figure directory", default="./figs")
     args = parser.parse_args()
     main(args)
