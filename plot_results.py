bf4e7436c85777e8beff8f68dd95cba7bfb7ffa7
Shion <smatsumoto97@gmail.com>
2024-11-25 07:15:15 -0500

    style plot

--- plot_results.py
+++ plot_results.py
@@ -9,6 +9,8 @@ from utils import read_json
 def main(args):
     os.makedirs(args.fig_dir, exist_ok=True)
     sns.set_theme()
+    sns.set_style("ticks")
+    sns.set_context("talk")
 
     params = read_json(f"{args.exp_dir}/params.json")
     metrics = read_json(f"{args.exp_dir}/metrics.json")
@@ -16,7 +18,7 @@ def main(args):
     data = []
     for epoch_idx, accuracies in enumerate(metrics["val_acc"]):
         for class_idx, accuracy in enumerate(accuracies):
-            data.append({'Epoch': epoch_idx, 'Class': f'{class_idx}', 'Accuracy': accuracy})
+            data.append({'Unlearning Epoch': epoch_idx, 'Class': f'{class_idx}', 'Accuracy': accuracy})
 
     df = pd.DataFrame(data)
     if params["forget_step"] and not params["retain_step"]:
@@ -28,15 +30,16 @@ def main(args):
     else:
         raise ValueError("Cannot have neither.")
 
-    fig, ax = plt.subplots(figsize=(15, 8))
-    sns.lineplot(data=df, x='Epoch', y='Accuracy', hue='Class', marker='o')
+    f, ax = plt.subplots(figsize=(10, 8))
+    sns.lineplot(data=df, x='Unlearning Epoch', y='Accuracy', hue='Class', marker='o')
 
-    plt.title(f"{params['dataset'].upper()} Per-Class Accuracy Over Unlearning Epochs ({setting} {params['forget_class']})")
-    plt.xticks(df["Epoch"])
-    plt.ylabel("Accuracy")
-    plt.tight_layout()
-    plt.grid()
-    plt.show()
+    ax.set_title(f"{params['dataset'].upper()} Per-Class Accuracy ({setting} {params['forget_class']})")
+    ax.set_xticks(df["Unlearning Epoch"])
+    ax.set_ylabel("Accuracy")
+    ax.grid()
+    f.tight_layout()
+
+    # plt.show()
 
     filename = f"{args.fig_dir}/unlearn_{params['dataset']}_{setting}_{os.path.basename(os.path.normpath(args.exp_dir))}.png"
     plt.savefig(filename, dpi=300)
