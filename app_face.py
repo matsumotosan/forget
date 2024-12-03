6509337122db28cb085030cd8f03e9b9b295bbb9
Shion <smatsumoto97@gmail.com>
2024-12-02 22:18:14 -0500

    mufac trained and unlearned preds

--- app_face.py
+++ app_face.py
@@ -1,136 +1,110 @@
 import torch
+import pandas as pd
 import random
-import numpy as np
 import streamlit as st
-from PIL import Image
-from face_classifier import FaceClassifier
-from lfw_classification_dataset import LFWClassificationDataset
-from facenet_pytorch import MTCNN
-from torchvision import transforms as T
 from streamlit_image_select import image_select
-from torch.nn.functional import softmax
+from unlearning_datamodule import mufac_transform
+from mufac_dataset import MUFAC
+from utils import mufac_class2idx
 
 
-DATASET_DIR = "forget_data"
-CKPT_PATH = "forget_downloads/version_38/checkpoints/epoch=19-step=520.ckpt"
+APP_DIR = "app_data"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-transform = T.Compose(
-    [
-        # T.ToTensor(),
-        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ]
-)
+EXPERIMENT_DIR = f"{APP_DIR}/2024-11-30-17-37-13"
+TRAINED_MODEL_PATH = f"{EXPERIMENT_DIR}/ckpt/epoch-0.pt"
+UNLEARNED_MODEL_PATH = f"{EXPERIMENT_DIR}/ckpt/epoch-20.pt"
+
+N_CHOICES = 12
 
 
 @st.cache_resource
-def load_classifier():
-    return FaceClassifier.load_from_checkpoint(CKPT_PATH).eval().to(device)
+def load_trained_model():
+    return torch.load(TRAINED_MODEL_PATH, map_location=device)
 
 
-def load_mtcnn():
-    mtcnn = MTCNN(
-        image_size=160,
-        margin=0,
-        min_face_size=20,
-        thresholds=[0.6, 0.7, 0.7],
-        factor=0.709,
-        post_process=True,
-        device=device,
-    )
-    return mtcnn
+@st.cache_resource
+def load_unlearned_model():
+    return torch.load(UNLEARNED_MODEL_PATH, map_location=device)
 
 
 @st.cache_resource
 def load_dataset():
-    with st.spinner("Downloading LFW dataset"):
-        dataset = LFWClassificationDataset(
-            root=DATASET_DIR,
-            split="train",
-            download=True,
-            people=["Ariel_Sharon", "Colin_Powell", "George_W_Bush"],
-            transform=transform,
-            preprocessed=False,
+    with st.spinner("Downloading MUFAC dataset"):
+        dataset = MUFAC(
+            root=APP_DIR,
+            stage="test",
         )
     return dataset
 
 
-def preprocess_image(image):
-    img = Image.open(image)
-    cropped = mtcnn(img)
-
-    st.write("What the model sees")
-    st.image(T.ToPILImage()(cropped))
-
-    if cropped is None:
-        st.error("Did not detect a face in the image.")
-    return transform(cropped)
-
-
-model = load_classifier()
-mtcnn = load_mtcnn()
+trained_model = load_trained_model()
+unlearned_model = load_unlearned_model()
 dataset = load_dataset()
 
-if "rand_idx" not in st.session_state:
-    st.session_state.rand_idx = random.sample(range(len(dataset.data)), 4)
-
-st.title("Face Identity Prediction")
+st.title("Face Attribute Classification")
 st.write(
-    "The application for which we are interested in exploring "
-    "machine unlearning is facial identification. The task inherently involves sensitive information "
-    "(images of people) that individuals may or may not have consented to the use of. Furthermore, "
+    "The application for which we are interested in exploring machine unlearning is facial attribute classification, specifically age classification. "
+    "The task inherently involves sensitive information in the form of images of individuals "
+    "that they may or may not have consented to the use of. Furthermore, "
     "once an image of a person has been used in training a facial recognition system, it is unclear "
     "if and how the individual's image can be eliminated from the model. For this reason, we are "
-    "interested in extending our findings from experimenting with the MNIST dataset to commonly used "
-    "facial recognition benchmarks such as Labeled Faces in the Wild (LFW)."
+    "interested in extending our findings from our experiments with the CIFAR-10 dataset to the a facial recognition dataset."
 )
 
 st.write(
-    "We chose to use the InceptionResnetV1 as our architecture for facial recognition given the ease "
-    "with which it can be adapted for a variety of face recognition tasks. The images are first "
-    "preprocessed by a Multitask Cascaded Convolutional Network (MTCNN) to detect and crop out the "
-    "face in the image (if it exists) prior to being passed to the InceptionResnetV1 model. We "
-    "trained the model from scratch on a modified version of the LFW dataset for classification."
+    "The facial recognition dataset that we are evaluating our unlearning strategy on is known as the Machine Unlearning for Facial Age Classification (MUFAC) dataset. "
+    "It is comprises of over 13,000 facial images of Asian individuals of various ages. Each image is labeled with the picture individual's age and identity, "
+    "allowing us to evaluate the effectiveness of both class- and instance-unlearning."
 )
 
-st.markdown(
-    ":red[If you see the error `IndexError: list index out of range`, please try refreshing the page or "
-    "interacting with a button on this page.]"
-)
-
+if "rand_idx" not in st.session_state:
+    st.session_state.rand_idx = random.sample(range(len(dataset.data)), N_CHOICES)
 if st.button("Get new choices"):
-    st.session_state.rand_idx = random.sample(range(len(dataset.data)), 4)
+    st.session_state.rand_idx = random.sample(range(len(dataset.data)), N_CHOICES)
 
 img = image_select(
     "Select an image to classify",
-    [dataset.data[i] for i in st.session_state.rand_idx],
+    [dataset[i][0] for i in st.session_state.rand_idx],
     use_container_width=False,
 )
 
-idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
-preprocessed_img = preprocess_image(img)
-logits = model(preprocessed_img.unsqueeze(0)).squeeze()
-probs = softmax(logits, dim=0).detach().numpy()
-topk = np.argsort(probs)
+x = mufac_transform(img).unsqueeze(0).to(device)
+
+trained_logits = trained_model(x)
+trained_probs = trained_logits.softmax(dim=1).squeeze().cpu().detach().numpy()
+
+unlearned_logits = unlearned_model(x)
+unlearned_probs = unlearned_logits.softmax(dim=1).squeeze().cpu().detach().numpy()
+
+df = pd.DataFrame(
+    {
+        "class": mufac_class2idx.keys(),
+        "trained": trained_probs,
+        "unlearned": unlearned_probs,
+    }
+)
 
 st.bar_chart(
-    data=probs[topk[:5]],
-    x_label="Probability",
-    horizontal=True,
+    data=df,
+    x="class",
+    y=["trained", "unlearned"],
+    x_label="Class",
+    y_label="Probability",
+    horizontal=False,
+    stack=False,
 )
 
-st.success(f"Predicted Identity: **{idx_to_class[topk[0]]}**")
-
+# Membership inference attack
+st.header("Membership Inference Attack")
 st.write(
-    "Note: Our model is collapsing during training to predict the class with the largest number of samples (Colin Powell). "
-    "We were unable to determine the cause of this but intend to address the issue for the final project."
+    "Although we have some evidence that the model is successfully unlearning, it is unclear if the model "
+    "has actually unlearned classes or if it is concealing its knowledge. Machine learning has demonstrated in numerous "
+    "scenarios that it is capable of exploiting features in the data to learn shortcuts instead of the intended solution."
+    "We used membership inference attack (MIA) to evaluate the extent to which a model scrubbed itself of memory of the classes intended to be forgotten.\n"
+    "The idea behind an MIA is to train an attacker (in our case a simple logistic regressor) on the losses of the model on inputs that were and were not part of the original training dataset. "
+    "If the attacker is able to distinguish between the two types of samples, that may be indicative of the model failing to adequately forget "
+    "classes to be forgotten."
 )
 
-st.subheader("Why train a model from scratch?")
-st.write(
-    "Although a pretrained model would provide us with a better starting point, "
-    "we thought that it would interfere with our future investigations into the unlearning process as a finetuned "
-    "model may retain additional knowledge compared to model we trained from scratch. "
-    "As such, we chose a popular architecture for facial recognition and decided to train it from "
-    "scratch as achieving state-of-the-art performance is not our end goal."
-)
+st.image("figs/mia_cifar10_unlearned.png")
